b498cf50145f43d9044d87006bfe208e770d2cc5
Orion <1343873415@qq.com>
2023-05-11 19:39:41 +0800

    add template

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,53 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { viteMockServe } from 'vite-plugin-mock'
+import { resolve } from 'path'
+
+const pathResolve = (dir) => {
+  return resolve(__dirname, ".", dir)
+}
+
+const alias = {
+  '@': pathResolve("src")
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default ({ command }) => {
+  const prodMock = true;
+  return {
+    base: './',
+    resolve: {
+      alias
+    },
+    server: {
+      port: 3002,
+      host: '0.0.0.0',
+      open: true,
+      proxy: { // 代理配置
+        '/dev': 'https://www.fastmock.site/mock/48cab8545e64d93ff9ba66a87ad04f6b/'
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            
+          }
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: command === 'serve',
+        prodEnabled: command !== 'serve' && prodMock,
+        watchFiles: true,
+        injectCode: `
+          import { setupProdMockServer } from '../mockProdServer';
+          setupProdMockServer();
+        `,
+        logger: true,
+      }),
+    ]
+  };
+}
